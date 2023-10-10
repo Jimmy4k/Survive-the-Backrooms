ff3f37f0fee11e5181cc1ee074f6464e00493b6a
Raphael Kempter <r.kempter88@gmail.com>
2023-10-10 19:36:20 +0200

    First Projekt commit, empty Project

---
+++ Assets/Scripts/flickerLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickerLight : MonoBehaviour {
+
+	public Light theLight;
+	//public GameObject lightObject;
+
+	[SerializeField]
+	float minTimeBeforeLightFlickers;
+	[SerializeField]
+	float maxTimeBeforeLightFlicker;
+
+	void Start ()
+	{
+		StartCoroutine("MakeLightFlicker");
+	}
+
+    IEnumerator MakeLightFlicker()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(minTimeBeforeLightFlickers, maxTimeBeforeLightFlicker));
+			theLight.enabled = !theLight.enabled;
+			//lightObject.SetActive(theLight.enabled);
+		}
+	} 
+}
