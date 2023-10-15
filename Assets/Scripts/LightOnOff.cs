62ee4121c4ca35b1dcffeaa0788f1e7abd1bf2b7
Raphael Kempter <r.kempter88@gmail.com>
2023-10-15 22:35:34 +0200

    Create Flicker Light Asset

---
+++ Assets/Scripts/LightOnOff.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LightOnOff : MonoBehaviour
+{
+    public AudioClip lightOnSound;
+    public AudioClip lightOffSound;
+    public AudioSource audioSource;
+
+    public GameObject[] lights;
+
+    public Material onMaterial;
+    public Material offMaterial;
+
+    public bool isOn; 
+    
+
+    private bool enterTrigger;
+
+    private void Update()
+    {
+        if (isOn == true)
+        {
+            LighOff();
+        }
+        else
+        {
+            LighOn();
+        }
+    }
+
+    public void LighOn()
+    {
+        foreach (GameObject light in lights)
+        {
+            {
+                MeshRenderer[] meshRenderer = light.GetComponentsInChildren<MeshRenderer>();
+                Light[] lightCompoment = light.GetComponentsInChildren<Light>();
+
+                foreach (MeshRenderer renderer in meshRenderer)
+                {
+                    renderer.material = onMaterial;
+                }
+                foreach (Light lichter in lightCompoment)
+                {
+                    audioSource.clip = lightOffSound;
+                    audioSource.Play();
+                    lichter.enabled = true;
+                }
+                isOn = true;
+            }
+        }
+    }
+    public void LighOff()
+    {
+        foreach (GameObject light in lights)
+        {
+            {
+                MeshRenderer[] meshRenderer = light.GetComponentsInChildren<MeshRenderer>();
+                Light[] lightCompoment = light.GetComponentsInChildren<Light>();
+
+                foreach (MeshRenderer renderer in meshRenderer)
+                {
+                    renderer.material = offMaterial;
+                }
+                foreach (Light lichter in lightCompoment)
+                {
+                    lichter.enabled = false;
+                    audioSource.clip = lightOffSound;
+                    audioSource.Play();
+                }
+                isOn = false;
+            }
+        } 
+    }
+}
--- Assets/Scripts/flickerLight.cs
+++ Assets/Scripts/flickerLight.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class flickerLight : MonoBehaviour {
 
-	public Light theLight;
-	//public GameObject lightObject;
+    public AudioClip lightSound;
+    public AudioSource audioSource;
 
-	[SerializeField]
+    public GameObject[] lights;
+
+    public GameObject cube;
+    public Material onMaterial;
+    public Material offMaterial;
+
+    public bool isOn;
+
+    [SerializeField]
 	float minTimeBeforeLightFlickers;
 	[SerializeField]
 	float maxTimeBeforeLightFlicker;
@@ -19,11 +28,59 @@ public class flickerLight : MonoBehaviour {
 
     IEnumerator MakeLightFlicker()
 	{
-		while (true)
-		{
-			yield return new WaitForSeconds(Random.Range(minTimeBeforeLightFlickers, maxTimeBeforeLightFlicker));
-			theLight.enabled = !theLight.enabled;
-			//lightObject.SetActive(theLight.enabled);
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minTimeBeforeLightFlickers, maxTimeBeforeLightFlicker));
+
+            if (isOn == true)
+            {
+                LighOff();
+            }
+            else
+            {
+                LighOn();
+            }
 		}
-	} 
+	}
+
+    public void LighOn()
+    {
+        foreach (GameObject light in lights)
+        {
+            {
+                
+                Renderer renderer = cube.GetComponent<Renderer>();
+                Material materials = renderer.material;
+                Light[] lightCompoment = light.GetComponentsInChildren<Light>();
+                foreach (Light lichter in lightCompoment)
+                {
+                    cube.GetComponent<MeshRenderer>().material = onMaterial;
+                    audioSource.clip = lightSound;
+                    audioSource.Play();
+                    lichter.enabled = true;
+                }
+                isOn = true;
+            }
+        }
+    }
+    public void LighOff()
+    {
+        foreach (GameObject light in lights)
+        {
+            {
+                Renderer renderer = cube.GetComponent<Renderer>();
+                Material materials = renderer.material;
+                Light[] lightCompoment = light.GetComponentsInChildren<Light>();
+
+                foreach (Light lichter in lightCompoment)
+                {
+                    cube.GetComponent<MeshRenderer>().material = offMaterial;
+                    lichter.enabled = false;
+                    audioSource.clip = lightSound;
+                    audioSource.Stop();
+                }
+                isOn = false;
+            }
+        }
+    }
 }
